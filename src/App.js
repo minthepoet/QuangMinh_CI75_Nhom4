1c1431c56faec195139d371683001db6c2bf35aa
unknown <minhthepoet@gmail.com>
2022-06-23 10:23:48 +0700

    add Day_3

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Section_1 from './components/Section_1'
+import Section_2 from './components/Section_2'
+
+
+const App = () =>{
+    return(
+        <div>
+            <Section_1 img = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Unknown_person.jpg/925px-Unknown_person.jpg"/>
+            <Section_2 />
+        </div>
+      )
+   
+
+}
+
+export default App;
